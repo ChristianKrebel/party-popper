0cdc72f646fc40b9350a2ca0a81a5a7536b1a639
starlesschris <chkrebel98@gmail.com>
2019-11-16 18:14:13 +0100

    add action bar items and change xml structure of dashboard

--- app/app/src/main/java/com/partypopper/app/features/dashboard/DashboardActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/dashboard/DashboardActivity.java
@@ -1,9 +1,17 @@
 package com.partypopper.app.features.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.partypopper.app.R;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -12,5 +20,57 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    /**
+     * Inflate the menu; this adds items to the action bar if it is present.
+     * @param menu
+     * @return if succeeded
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handle item selection of the menu
+     * @param item
+     * @return if succeeded or with item on default
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        CharSequence text;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                text = "Settings";
+                testToast(text);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Print a toast for testing purposes
+     * @param text
+     */
+    protected void testToast(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }

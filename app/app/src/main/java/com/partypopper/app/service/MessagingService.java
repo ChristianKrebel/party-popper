c0c052304bff8592655b5e5b16381297a091d06a
Joyce <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2019-12-18 13:29:02 +0100

    added classes

--- app/app/src/main/java/com/partypopper/app/service/MessagingService.java
+++ app/app/src/main/java/com/partypopper/app/service/MessagingService.java
@@ -1,11 +1,60 @@
 package com.partypopper.app.service;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MessageingService";
+    private static final String TAG = "MessagingService";
 
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        sendRegistrationToServer(token);
+    }
 
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
 
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        Log.d(TAG, "From: " + message.getFrom());
+
+        // Check if message contains a data payload.
+        if (message.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + message.getData());
+
+            /*if (true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }*/
+
+        }
+
+        // Check if message contains a notification payload.
+        if (message.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + message.getNotification().getBody());
+        }
+    }
 }

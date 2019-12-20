dfd0e8abb2c24774875a9b7e089ca71aac7329cf
Joyce Marvin Rafflenbeul <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2019-12-20 01:07:43 +0100

    minor fix

--- app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
@@ -17,8 +17,6 @@ public class BusinessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

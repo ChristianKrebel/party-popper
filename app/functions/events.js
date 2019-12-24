2fc05df5ca46037e71ddf58f7af79cfdf795d09f
Leon338 <Leon.Hornig@web.de>
2019-12-24 21:42:00 +0100

    Finished Events Join

--- app/functions/events.js
+++ app/functions/events.js
@@ -1,40 +1,71 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const db = admin.firestore();
+const helper = require("./functions");
+
 exports.joinEvent = functions.https.onCall(async (data, context) => {
   helper.errorIfEmpty(data.eventId);
   helper.errorIfNotAuthenticated(context);
 
   const uid = context.auth.uid;
   const eventDoc = db.collection("events").doc(data.eventId);
-  const joinedRef = eventDoc.collection("joined");
-
   try {
-    const follower = await followerDoc.get();
-    if (follower.exists && follower.data().follower.includes(uid)) {
+    const event = await eventDoc.get();
+    if (!event.exists) {
+      return false;
+    }
+
+    const joinedDoc = eventDoc.collection("joined").doc(uid);
+    const joined = await joinedDoc.get();
+    if (!joined.exists) {
       const promises = [];
 
-      // Add to Follower
       promises.push(
-        followerDoc.update(
-          "follower",
-          admin.firestore.FieldValue.arrayRemove(uid)
-        )
+        joinedDoc.set({
+          uid: uid,
+          joinedAt: admin.firestore.FieldValue.serverTimestamp()
+        })
       );
 
-      // Increment Follower Count
       promises.push(
-        orgDoc.update("followCount", admin.firestore.FieldValue.increment(-1))
+        eventDoc.update("going", admin.firestore.FieldValue.increment(1))
       );
 
-      // Add to Following
-      promises.push(
-        followingDoc.update(
-          "following",
-          admin.firestore.FieldValue.arrayRemove(data.orgId)
-        )
-      );
+      await Promise.all(promises);
+      return true;
+    }
+  } catch (err) {
+    console.log(err);
+    throw new functions.https.HttpsError(
+      "internal",
+      "An internal Error occured"
+    );
+  }
+  return false;
+});
 
-      // Increment Following Count
+exports.leaveEvent = functions.https.onCall(async (data, context) => {
+  helper.errorIfEmpty(data.eventId);
+  helper.errorIfNotAuthenticated(context);
+
+  const uid = context.auth.uid;
+  const eventDoc = db.collection("events").doc(data.eventId);
+  try {
+    const event = await eventDoc.get();
+    if (!event.exists) {
+      return false;
+    }
+
+    const joinedDoc = eventDoc.collection("joined").doc(uid);
+    const joined = await joinedDoc.get();
+    if (joined.exists) {
+      const promises = [];
+
+      promises.push(joinedDoc.delete());
+
       promises.push(
-        userDoc.update("following", admin.firestore.FieldValue.increment(-1))
+        eventDoc.update("going", admin.firestore.FieldValue.increment(-1))
       );
 
       await Promise.all(promises);

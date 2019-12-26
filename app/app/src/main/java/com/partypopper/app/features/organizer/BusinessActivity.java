3e6829e8a0512ee6f101bc40ef5db3bfbd6c8fd3
Joyce Marvin Rafflenbeul <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2019-12-26 22:03:04 +0100

    activity updates

--- app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
@@ -2,6 +2,7 @@ package com.partypopper.app.features.organizer;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,6 +10,10 @@ import android.widget.Toast;
 import com.partypopper.app.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class BusinessActivity extends AppCompatActivity {
     @Override
@@ -37,6 +42,19 @@ public class BusinessActivity extends AppCompatActivity {
     public void sendRequest(View btn){
         EditText businessNameWdg = findViewById(R.id.bsBusinessnamePt);
         String businessName = businessNameWdg.getText().toString();
-        testToast(businessName);
+        EditText businessWebsiteWdg = findViewById(R.id.bsBusinesswebsitePt);
+        String businessWebsite = businessWebsiteWdg.getText().toString();
+        EditText businessPhoneWdg = findViewById(R.id.bsBusinessphoneP);
+        String businessTelephone = businessPhoneWdg.getText().toString();
+        EditText businessAddressWdg = findViewById(R.id.bsBusinessaddressPa);
+        String businessAddress = businessAddressWdg.getText().toString();
+        EditText businessDescriptionWdg = findViewById(R.id.bsDescriptionMt);
+        String businessDescription = businessDescriptionWdg.getText().toString();
+        EditText businessEmailWdg = findViewById(R.id.bsBusinessmailE);
+        String businessEmail = businessEmailWdg.getText().toString();
+
+        // TODO sending post request?
     }
+
+
 }

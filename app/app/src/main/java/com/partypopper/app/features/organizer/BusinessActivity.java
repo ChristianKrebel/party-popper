12c65e6fbdfcabaab00104e81d6e56de0fbc3599
Joyce Marvin Rafflenbeul <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2020-01-03 01:38:51 +0100

    Docs Einleitung Progress

--- app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/organizer/BusinessActivity.java
@@ -1,13 +1,20 @@
 package com.partypopper.app.features.organizer;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,21 +24,30 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.partypopper.app.R;
 import com.partypopper.app.utils.BaseActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 public class BusinessActivity extends BaseActivity implements OnMapReadyCallback {
     private String businessAddress = "";
+    private ImageView imageView;
+    private Button btnUpload;
     private EditText businessAddressWdg;
     private MapFragment organizerLocationMf;
     private GoogleMap map;
     private LatLng addressPoint;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +66,9 @@ public class BusinessActivity extends BaseActivity implements OnMapReadyCallback
         // get MapFragment
         organizerLocationMf = (MapFragment) getFragmentManager().findFragmentById(R.id.bsMapMv);
         organizerLocationMf.getMapAsync(this);
+
+        btnUpload = findViewById(R.id.bsUpdateBtn);
+        imageView = findViewById(R.id.bsLogoImg);
 
         EditText businessNameWdg = findViewById(R.id.bsBusinessnamePt);
         String businessName = businessNameWdg.getText().toString();
@@ -156,13 +175,74 @@ public class BusinessActivity extends BaseActivity implements OnMapReadyCallback
         map = googleMap;
     }
 
-    public LatLng getAddressPoint() {
-        return addressPoint;
-    }
-
+    /**
+     * reloads Google Map on call
+     */
     public void reloadMap() {
         MapFragment organizerLocationMfReload = (MapFragment) getFragmentManager().findFragmentById(R.id.bsMapMv);
         organizerLocationMfReload.getMapAsync(this);
     }
 
+    /**
+     * Defining Implicit Intent to mobile gallery
+     */
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * Override onActivityResult method to check request code and result code and to set image into the image view
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContentResolver(),
+                                filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Method to upload the chosen image to Firestore
+     */
+    private void uploadImage() {
+        if (filePath != null) {
+            ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            //TODO implement Firestore Connection
+        }
+    }
+
+    public LatLng getAddressPoint() {
+        return addressPoint;
+    }
 }
--- docs/_sidebar.md
+++ docs/_sidebar.md
@@ -1,2 +1,8 @@
 * <b> Table of Contents </b>
-  * [Einleitung](/markdown/introduction.md)
+  * [Einleitung](/markdown/einleitung.md)
+  * [Stand der Technik](/markdown/stand_der_technik.md)
+  * [Anforderungsdokumentation](/markdown/anforderungsdokumentation.md)
+  * [Architekturbeschreibung](/markdown/architekturbeschreibung.md)
+  * [Implementierung](/markdown/implementierung.md)
+  * [Test und Usability](/markdown/test_und_usability.md)
+  * [Zusammenfassung](/markdown/zusammenfassung.md)

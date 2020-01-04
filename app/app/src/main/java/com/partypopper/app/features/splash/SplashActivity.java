6f0a7bdefc5b1b4d79c9a97a1eae749a89032771
starlesschris <chkrebel98@gmail.com>
2020-01-04 01:42:35 +0100

    show organizer ui

--- app/app/src/main/java/com/partypopper/app/features/splash/SplashActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/splash/SplashActivity.java
@@ -36,41 +36,11 @@ public class SplashActivity extends BaseActivity {
                 @Override
                 public void onSuccess(GetTokenResult result) {
                     if(result.getClaims().get("organizer") != null) {
-                        /*Event event = new Event();
-                        event.setName("Bla bla bla");
-                        event.setOrganizer(currentUser.getUid());
-                        event.setGoing(0);
-                        event.setStartDate(new Date(2019, 12, 31));
-                        event.setEndDate(new Date(2020, 1, 1));
-                        event.setDescription("ASFBjhfsdaghwhzeugw fesdahfewe");
-                        event.setLowercaseName(event.getName().toLowerCase());
-                        event.setImage("https://dgfiugrtherwergt");
-
-                        OrganizerRepository repo = OrganizerRepository.getInstance();
-
-                        repo.createEvent(event).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                System.out.println("EVENT created");
-                            }
-                        });*/
-
-                        /*EventsRepository repo = EventsRepository.getInstance();
-                        repo.searchByName("bla").addOnCompleteListener(new OnCompleteListener<List<Event>>() {
-                            @Override
-                            public void onComplete(@NonNull Task<List<Event>> task) {
-                                if(task.getResult() != null) {
-                                    List<Event> list = task.getResult();
-
-                                    System.out.println(list);
-                                }
-                            }
-                        });*/
-
                         showOrganizerUI();
-                    } else {
-                        showUserUI();
                     }
+                    Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
                 }
             });
         }
@@ -79,12 +49,6 @@ public class SplashActivity extends BaseActivity {
     private void showOrganizerUI() {
         System.out.println("Is Organizer");
         DashboardActivity.setOrganizer(true);
-    }
-
-    private void showUserUI() {
-        Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
     }
 
     private void showLoginUI() {

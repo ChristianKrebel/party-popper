4df83e56e1f232bef35ebbefa726d3a9f1bbd715
Joyce Marvin Rafflenbeul <joyce_marvin.rafflenbeul@fh-bielefeld.de>
2020-01-04 03:24:50 +0100

    Progress BusinessActivity + PublishEventActivity + Image Upload

--- app/app/src/main/java/com/partypopper/app/features/organizer/PublishEventActivity.java
+++ app/app/src/main/java/com/partypopper/app/features/organizer/PublishEventActivity.java
@@ -1,6 +1,11 @@
 package com.partypopper.app.features.organizer;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,10 +13,19 @@ import android.widget.ImageView;
 import android.widget.TimePicker;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.functions.HttpsCallableResult;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.partypopper.app.R;
 import com.partypopper.app.database.model.Event;
+import com.partypopper.app.database.model.Organizer;
 import com.partypopper.app.database.repository.EventsRepository;
 import com.partypopper.app.database.repository.OrganizerRepository;
 import com.partypopper.app.utils.BaseActivity;
@@ -21,11 +35,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 
 public class PublishEventActivity extends BaseActivity {
+    // File Upload attributes
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+
     private EditText publisheventTitleWdg;
     private EditText publisheventStartdateWdg;
     private EditText publisheventEnddateWdg;
@@ -50,6 +72,9 @@ public class PublishEventActivity extends BaseActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);  // set back button
         actionBar.setDisplayShowHomeEnabled(true);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         DatePicker dp1 = new DatePicker(this, R.id.pubStartdateD);
         DatePicker dp2 = new DatePicker(this, R.id.pubEnddateD);
@@ -77,47 +102,120 @@ public class PublishEventActivity extends BaseActivity {
     }
 
     public void onPublishClick(View view) {
-        String eventTitle = publisheventTitleWdg.getText().toString();
-        String eventStartdate = publisheventStartdateWdg.getText().toString();
-        String eventEnddate = publisheventEnddateWdg.getText().toString();
-        int eventStarttimeHour = publisheventStarttimeWdg.getHour();
-        int eventStarttimeMinute = publisheventStarttimeWdg.getMinute();
-        int eventEndtimeHour = publisheventEndtimeWdg.getHour();
-        int eventEndtimeMinute = publisheventEndtimeWdg.getMinute();
-        String eventDescription = publisheventDescriptionWdg.getText().toString();
+        //TODO validate Time Input
+        if(filePath != null && !publisheventTitleWdg.getText().toString().isEmpty()
+                && !publisheventStartdateWdg.getText().toString().isEmpty()
+                && !publisheventEnddateWdg.getText().toString().isEmpty()
+                && !publisheventDescriptionWdg.getText().toString().isEmpty()) {
+            uploadImage();
+            String eventTitle = publisheventTitleWdg.getText().toString();
+            String eventStartdate = publisheventStartdateWdg.getText().toString();
+            String eventEnddate = publisheventEnddateWdg.getText().toString();
+            // Angeblich zu niedriges API Level, obwohl offizielle Android Doku API Level 1 ansagt
+            int eventStarttimeHour = publisheventStarttimeWdg.getHour();
+            int eventStarttimeMinute = publisheventStarttimeWdg.getMinute();
+            int eventEndtimeHour = publisheventEndtimeWdg.getHour();
+            int eventEndtimeMinute = publisheventEndtimeWdg.getMinute();
+            String eventDescription = publisheventDescriptionWdg.getText().toString();
 
-        String[] startDateArr = eventStartdate.split("/");
-        int startDateDay = Integer.parseInt(startDateArr[0].trim());
-        int startDateMonth = Integer.parseInt(startDateArr[1].trim());
-        int startDateYear = Integer.parseInt(startDateArr[2].trim());
-        //showText(startDateDay + " " + startDateMonth + " " + startDateYear);
+            String[] startDateArr = eventStartdate.split("/");
+            int startDateDay = Integer.parseInt(startDateArr[0].trim());
+            int startDateMonth = Integer.parseInt(startDateArr[1].trim());
+            int startDateYear = Integer.parseInt(startDateArr[2].trim());
+            //showText(startDateDay + " " + startDateMonth + " " + startDateYear);
 
-        String[] endDateArr = eventEnddate.split("/");
-        int endDateDay = Integer.parseInt(endDateArr[0].trim());
-        int endDateMonth = Integer.parseInt(endDateArr[1].trim());
-        int endDateYear = Integer.parseInt(endDateArr[2].trim());
+            String[] endDateArr = eventEnddate.split("/");
+            int endDateDay = Integer.parseInt(endDateArr[0].trim());
+            int endDateMonth = Integer.parseInt(endDateArr[1].trim());
+            int endDateYear = Integer.parseInt(endDateArr[2].trim());
 
-        Event event = new Event();
-        event.setName(eventTitle);
-        event.setOrganizer(currentUser.getUid());
-        event.setGoing(0);
-        event.setStartDate(new Date(startDateYear, startDateMonth, startDateDay, eventStarttimeHour, eventStarttimeMinute));
-        event.setEndDate(new Date(endDateYear, endDateMonth, endDateDay, eventEndtimeHour, eventEndtimeMinute));
-        event.setDescription(eventDescription);
-        event.setLowercaseName(event.getName().toLowerCase());
-        event.setImage("https://dgfiugrtherwergt");
+            OrganizerRepository repo = OrganizerRepository.getInstance();
+            Event event = new Event();
+            event.setName(eventTitle);
+            event.setOrganizer(currentUser.getUid());
+            event.setGoing(0);
+            event.setStartDate(new Date(startDateYear, startDateMonth, startDateDay, eventStarttimeHour, eventStarttimeMinute));
+            event.setEndDate(new Date(endDateYear, endDateMonth, endDateDay, eventEndtimeHour, eventEndtimeMinute));
+            event.setDescription(eventDescription);
+            event.setLowercaseName(event.getName().toLowerCase());
+            event.setImage(filePath.toString());
 
-
-        EventsRepository repo = EventsRepository.getInstance();
-        repo.searchByName("bla").addOnCompleteListener(new OnCompleteListener<List<Event>>() {
-            @Override
-            public void onComplete(@NonNull Task<List<Event>> task) {
-                if(task.getResult() != null) {
-                    List<Event> list = task.getResult();
-
-                    System.out.println(list);
+            repo.createEvent(event).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    System.out.println("EVENT created");
                 }
+            });
+        }
+    }
+
+    /**
+     * Defining Implicit Intent to mobile gallery
+     */
+    public void chooseImage(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * Override onActivityResult method to check request code and result code and to set image into the image view
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null ) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                publisheventImageWdg.setImageBitmap(bitmap);
             }
-        });
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Method to upload the chosen image to Firestore
+     */
+    public void uploadImage() {
+        if (filePath != null) {
+            final ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("events/"+ UUID.randomUUID().toString() + ".jpg");
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            showText("Uploaded");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            showText("Upload failed");
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+            showText(ref.getPath());
+        }
     }
 }
